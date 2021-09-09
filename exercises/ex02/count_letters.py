703b9dcf2c45f3da9833dc15ad62ce59d6cf30f0
Rodrigo Hernandez-Farrera <rodrigor@live.unc.edu>
2021-09-08 21:33:14 -0400

    Progress on Exercise 2

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,17 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730400384"
 
 
 # Begin your solution here...
+searched_letter = str(input("What letter do you want to search for?: "))
+word_entered = str(input("Enter a word: "))
+i = 0
+length_of_word = int(len(word_entered))
+amount_of_letters = 0
+
+while i < length_of_word:
+    if searched_letter == word_entered[i]:
+        amount_of_letters = amount_of_letters + 1
+    i = i + 1
+print("Count: " + str(amount_of_letters))
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,20 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730400384"
 
 
-# Begin your solution here...
+repeated_word: str = input("What beat do you want to repeat? ")
+times: int = int(input("How many times do you want to repeat it? "))
+i = 0
+wanted_string = ""
+
+if times <= 0:
+    print("No beat...")
+else:
+    while i < times:
+        if i == times - 1:
+            wanted_string = wanted_string + str(repeated_word)
+        else:
+            wanted_string = wanted_string + str(repeated_word) + " "
+        i = i + 1
+    print(wanted_string)
